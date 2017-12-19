9a00f3fea86499ca44d97d892724c47cb189dee5
tut173330 <s173330@edu.tut.ac.jp>
2017-12-19 16:03:16 +0900

    Hello.java_modified

--- s4/b173330/Hello.java
+++ s4/b173330/Hello.java
@@ -2,6 +2,7 @@ package s4.b173330;
 import java.lang.*;
 
 public class Hello {
+    /* Hello.java_Modified */
     public static void main(String[] args) {
 	System.out.println("hello..(buggy)");
     }
