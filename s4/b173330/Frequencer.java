58a9a1c27033a4e6e8a57c92b7dfe720891034ca
tut173330 <s173330@edu.tut.ac.jp>
2018-02-09 18:30:26 +0900

    modified

--- s4/b173330/Frequencer.java
+++ s4/b173330/Frequencer.java
@@ -1,4 +1,5 @@
 package s4.b173330; // Please modify to s4.Bnnnnnn, where nnnnnn is your student ID. 
+
 import java.lang.*;
 import s4.specification.*;
 
@@ -14,180 +15,267 @@ interface FrequencerInterface {     // This interface provides the design for fr
     // For the incorrect value of START or END, the behavior is undefined.
 */
 
+public class Frequencer implements FrequencerInterface {
+	// Code to Test, *warning: This code  contains intentional problem*
+	byte[] myTarget;
+	byte[] mySpace;
+	boolean targetReady = false;
+	boolean spaceReady = false;
+	int[] suffixArray;
 
-public class Frequencer implements FrequencerInterface{
-    // Code to Test, *warning: This code  contains intentional problem*
-    byte [] myTarget;
-    byte [] mySpace;
-    boolean targetReady = false;
-    boolean spaceReady = false;
+	private void printSuffixArray() {
+		System.out.println("[Suffix]");
+		if (spaceReady) {
+			for (int i = 0; i < mySpace.length; i++) {
+				int s = suffixArray[i];
+				for (int j = s; j < mySpace.length; j++) {
+					System.out.write(mySpace[j]);
+				}
+				System.out.write('\n');
+			}
+		}
+	}
 
-    int [] suffixArray;
-    /* public void setTarget(byte[] target) { myTarget = target;}
-       public void setSpace(byte[] space) { mySpace = space; }*/
+	private int suffixCompare(int i, int j) {
+		int si = suffixArray[i];
+		int sj = suffixArray[j];
+		int s = 0;
+		if (si > s)
+			s = si;
+		if (sj > s)
+			s = sj;
+		int n = mySpace.length - s;
+		for (int k = 0; k < n; k++) {
+			if (mySpace[si + k] > mySpace[sj + k]){
+				return 1;
+			}
+			if (mySpace[si + k] < mySpace[sj + k]){
+				return -1;
+			}
+		}
+		if (si < sj){
+			return 1;
+		}
+		if (si > sj){
+			return -1;
+		}
+		return 0;
+	}
 
-    private void printSuffixArray(){
-	if(spaceReady) {
-	    for(int i=0; i<mySpace.length; i++){
+	public void setSpace(byte[] space) {
+		mySpace = space;
+		if (mySpace.length > 0)
+			spaceReady = true;
+		suffixArray = new int[space.length];
+		for (int i = 0; i < space.length; i++) {
+			suffixArray[i] = i;
+		}
+
+		/*
+		for (int i = 0; i < space.length; i++) {
+			for (int j = i + 1; j < space.length; j++) {
+				if (suffixCompare(i, j) == 1) {
+					int tmp = suffixArray[i];
+					suffixArray[i] = suffixArray[j];
+					suffixArray[j] = tmp;
+				}
+			}
+		}
+		*/
+
+		// LocalClass for QuicSort Algorythm.
+		class QuickSort{
+			
+			public void quickSort(int[] suffixArray, int left, int right) {
+				int curleft = left;
+				int curright = right;
+				int pivot = (curleft + curright) / 2;
+
+				do {
+					while (suffixCompare(curleft , pivot) == -1) {
+						curleft++;
+					}
+					
+					while (suffixCompare(pivot, curright) == -1) {
+						curright--;
+					}
+					if (curleft <= curright) {
+						swap (suffixArray, curleft++, curright--);
+					}
+				} while (curleft < curright);
+				
+				if (left < curright) {
+					quickSort(suffixArray, left, curright);
+				}
+				
+				if (curleft < right) {
+					quickSort(suffixArray, curleft, right);
+				}
+			}
+
+			private void swap (int[] array,  int idx1, int idx2) {
+				int tmp = array[idx1];
+				array[idx1] = array[idx2];
+				array[idx2] = tmp;
+			}
+		}
+
+		QuickSort myQuickSort = new QuickSort();
+		myQuickSort.quickSort(suffixArray, 0, space.length-1);
+
+		printSuffixArray();
+	}
+
+	private int targetCompare(int i, int start, int end) {
 		int s = suffixArray[i];
-		for(int j=s; j<mySpace.length; j++){
-		    System.out.write(mySpace[j]);
+		int tl = end - start;
+		if (tl > mySpace.length - s)
+			return -1;
+		for (int k = 0; k < tl; k++) {
+			if (mySpace[s + k] > myTarget[start + k])
+				return 1;
+			if (mySpace[s + k] < myTarget[start + k])
+				return -1;
 		}
-		System.out.write('\n');
-	    }
+		return 0;
 	}
-    }
 
-    private int suffixCompare(int i,int j) {
-	int si = suffixArray[i];
-	int sj = suffixArray[j];
-	int s = 0;
-	if(si > s) s = si;
-	if(sj > s) s = sj;
-	int n = mySpace.length -s;
-	for(int k = 0; k<n; k++){
-	    if(mySpace[si+k]>mySpace[sj+k]) return 1;
-	    if(mySpace[si+k]<mySpace[sj+k]) return -1;
-	}
-	if(si < sj) return 1;
-	if(si > sj) return -1;
-	return 0;
-    }
-
-    public void setSpace(byte[] space) {
-	mySpace = space;
-	if(mySpace.length>0)
-	    spaceReady = true;
-	suffixArray = new int[space.length];
-	for(int i = 0; i < space.length; i++){
-	    suffixArray[i] = i;
-	}
-	
-	//myspace[i]~myspace[end]とmyspace[j]~myspace[end]を比較して,結果に応じてsuffixArrayを交換するようにする
-	for(int i=0;i<space.length;i++){
-	    for(int j=i+1;j<space.length;j++){
-		if(suffixCompare(i,j)==1){
-		    int tmp=suffixArray[i];
-		    suffixArray[i]=suffixArray[j];
-		    suffixArray[j]=tmp;
+	private int subByteStartIndex(int start, int end) {
+		/*
+		for (int i = 0; i < mySpace.length; i++) {
+			if (targetCompare(i, start, end) == 0) {
+				return i;
+			}
 		}
-	    }
-	}
-	printSuffixArray();
-    }
+		return suffixArray.length;
+		*/
 
-    private int targetCompare(int i,int start,int end){
-        int s = suffixArray[i];
-        int tl = end-start;
-        if(tl > mySpace.length-s) return -1;
-        for(int k = 0; k<tl; k++){
-            if(mySpace[s+k]>myTarget[start+k]) return 1;
-            if(mySpace[s+k]<myTarget[start+k]) return -1;
-        }
-        return 0;
-    }
-    
-    
-    private int subByteStartIndex(int start,int end){
-	for(int i=0;i<mySpace.length;i++){
-	    if(targetCompare(i,start,end)==0){
-		return i;
-	    }
+		int target_stut;
+		int pLeft = 0;
+		int pRight = suffixArray.length-1;
+		int center = (pLeft+pRight)/2;
+		while((target_stut = targetCompare(center, start, end)) != 0){
+			System.out.println("space_pos : "+center);
+			if((center == 0) || (center == suffixArray.length-1)){
+				return center;
+			}
+			switch(target_stut){
+				case -1:
+					System.out.println("right");
+					pLeft = center+1;
+					center = (pLeft + pRight)/2;
+					break;
+				case 1:
+					System.out.println("left");
+					pRight = center-1;
+					center = (pLeft + pRight)/2;
+					break;
+			}
+		}
+
+		while((target_stut = targetCompare(center, start, end)) == 0){
+			center--;
+			if(center == -1){
+				return -1;
+			}
+		}
+		return center;
 	}
-	return suffixArray.length;
-    }
-    
-    private int subByteEndIndex(int start,int end){
-	for(int i=mySpace.length-1;i>0;i--){
-	    if(targetCompare(i,start,end)==0)
-		return i+1;
+
+	private int subByteEndIndex(int start, int end) {
+		/*
+		for (int i = mySpace.length - 1; i > 0; i--) {
+			if (targetCompare(i, start, end) == 0)
+				return i + 1;
+		}
+		return suffixArray.length;
+		*/
+
+		int target_stut;
+		int pLeft = 0;
+		int pRight = suffixArray.length-1;
+		int center = (pLeft+pRight)/2;
+		while((target_stut = targetCompare(center, start, end)) != 0){
+			if((center == 0) || (center == suffixArray.length-1)){
+				return center;
+			}
+			switch(target_stut){
+				case -1:
+					pLeft = center+1;
+					center = (pLeft + pRight)/2;
+					break;
+				case 1:
+					pRight = center-1;
+					center = (pLeft + pRight)/2;
+					break;
+			}
+		}
+
+		while(((target_stut = targetCompare(center, start, end)) == 0)){
+			center++;
+			if(center == suffixArray.length){
+				break;
+			}
+		}
+		return center-1;
 	}
-	return suffixArray.length;
-    }
-    
-    public int subByteFrequency(int start,int end){
-	int spaceLength = mySpace.length;
-	int count = 0;
-	for(int offset = 0; offset<spaceLength - (end - start); offset++){
-	    boolean abort = false;
-	    for(int i=0; i< (end-start); i++){
-		if(myTarget[start+i] != mySpace[offset+i])
-		    {
-			abort = true;
-			break;
-		    }
-	    }
-	    if(abort == false)
-		{
-		    count++;
+
+	public int subByteFrequency(int start, int end) {
+		int spaceLength = mySpace.length;
+		int count = 0;
+		for (int offset = 0; offset < spaceLength - (end - start); offset++) {
+			boolean abort = false;
+			for (int i = 0; i < (end - start); i++) {
+				if (myTarget[start + i] != mySpace[offset + i]) {
+					abort = true;
+					break;
+				}
+			}
+			if (abort == false) {
+				count++;
+			}
+		}
+
+		System.out.println("first");
+		int first = subByteStartIndex(start, end);
+		System.out.println("first : "+first);
+		System.out.println("end");
+		int last1 = subByteEndIndex(start, end);
+		System.out.println("end : "+last1);
+		return last1 - first;
+	}
+
+	public void setTarget(byte[] target) {
+		myTarget = target;
+		if (myTarget.length > 0)
+			targetReady = true;
+	}
+
+	public int frequency() {
+		if (targetReady == false)
+			return -1;
+		if (spaceReady == false)
+			return 0;
+		return subByteFrequency(0, myTarget.length);
+	}
+
+	public static void main(String[] args) {
+		Frequencer myObject;
+		int freq;
+		try {
+			System.out.println("checking my Frequencer");
+			myObject = new Frequencer();
+			myObject.setSpace("Hi Ho Hi Ho".getBytes());
+			myObject.setTarget("H".getBytes());
+			freq = myObject.frequency();
+			System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears " + freq + " times. ");
+			if (4 == freq) {
+				System.out.println("OK");
+			} else {
+				System.out.println("WRONG");
+			}
+		} catch (Exception e) {
+			System.out.println("Exception occurred: STOP");
 		}
 	}
-        
-	int first = subByteStartIndex(start,end);
-	int last1 = subByteEndIndex(start,end);
-	return last1-first;
-    }
-    
-    public void setTarget(byte[] target) {
-	myTarget = target;
-	if(myTarget.length>0)
-	    targetReady=true;
-    }
-    
-    public int frequency() {
-	if(targetReady==false)return -1;
-	if(spaceReady==false)return 0;
-	return subByteFrequency(0,myTarget.length);
-    }
-    
-    /* public int frequency() {
-       
-       if(mySpace == null || mySpace.length == 0){
-       return 0;
-       }
-       
-       if(myTarget == null ||  myTarget.length == 0){
-       return -1;
-       }
-	
-       
-       int targetLength = myTarget.length;
-       int spaceLength = mySpace.length;
-       int count = 0;
-       
-       for(int start = 0; start<spaceLength; start++) {
-	    boolean abort = false;
-	    for(int i = 0; i<targetLength; i++) {
-	    if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
-	    }
-	    if(abort == false) { count++; }
-	    }
-	    return count;
-	    }
-	    
-	    // I know that here is a potential problem in the declaration.
-	    public int subByteFrequency(int start, int length) { 
-	    // Not yet, but it is not currently used by anyone.
-	    return -1;
-	    }/*/
-    
-    public static void main(String[] args) {
-	Frequencer myObject;
-	int freq;
-	try {
-	    System.out.println("checking my Frequencer");
-	    myObject = new Frequencer();
-	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
-	    
-	    myObject.setTarget("H".getBytes());
-        System.out.println("settarget");
-	    freq = myObject.frequency();
-        System.out.println("frequency");
-	    System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
-	    if(4 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
-	}
-	catch(Exception e) {
-	    System.out.println("Exception occurred: STOP");
-	}
-    }
-}	  
+}
